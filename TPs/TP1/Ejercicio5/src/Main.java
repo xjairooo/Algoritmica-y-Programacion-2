5fc61c7f698de89273d525a8cd889ae6e651e773
Jairo Campero <camperojairo0@gmail.com>
2024-12-28 19:06:23 -0300

    Ejercicio 4 in coming...

---
+++ TPs/TP1/Ejercicio5/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese 2 numeros: ");
+        int num1 = scanner.nextInt(), num2 = scanner.nextInt();
+
+    }
+
+}
