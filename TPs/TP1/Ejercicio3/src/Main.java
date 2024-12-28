9eaa34be2c641703c1459fc25787118b6eac6986
Jairo Campero <camperojairo0@gmail.com>
2024-12-28 16:28:26 -0300

    Ejercio 3 completado

---
+++ TPs/TP1/Ejercicio3/src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * 3. Escribir un programa que lee dos números e imprima el número más grande
+ * seguido de las palabras es más grande. Si son iguales, imprimir el mensaje
+ * los números son iguales
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese dos numeros: ");
+        int num1 = scanner.nextInt(), num2 = scanner.nextInt();
+        System.out.println(num1 == num2 ? "Los números son iguales" :
+                (num1 > num2 ? num1 : num2) + " es más grande");
+    }
+}
