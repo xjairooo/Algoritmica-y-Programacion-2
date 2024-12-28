4dbc15de3d6573bfdf002ac009b59692fad2e4cd
Jairo Campero <camperojairo0@gmail.com>
2024-12-28 16:40:25 -0300

    Ejercicio 4 completado

---
+++ TPs/TP1/Ejercicio4/src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 4. Escribir un programa que lee un número e imprima si es par o impar
+ * utilizando el operador módulo.
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa un numero: ");
+        int num1 = scanner.nextInt();
+        // Verifica si es par o impar
+        System.out.println(num1 % 2 == 0 ? "Es par" : "Es impar");
+    }
+}
