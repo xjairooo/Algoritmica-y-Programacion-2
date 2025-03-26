f6bc22fed7a706c0a1005397d062c85be8322df2
xjairooo <camperojairo0@gmail.com>
2025-03-26 17:22:08 -0300

    cambios

--- Trabajos Practicos/TP2/Hola.java
+++ Trabajos Practicos/TP2/Hola.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Hola {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("Hola Mundo");
+
+        System.out.println("Ingrese su nombre: ");
+        String nombre = input.nextLine();
+        System.out.println("Hola " + nombre + "!");
+        input.close();
+
     }
 }
